53433fa27aad28c235a5e623d1b212f0b574d9e8
ncolonna <colonnan96@gmail.com>
2021-05-21 15:47:59 -0400

    attempting a delete button

--- server.py
+++ server.py
@@ -22,20 +22,28 @@ class Posts(db.Model):
 # Routes
 @app.route('/', methods=["GET", "POST"])
 def index():
-	if request.method == "GET":
-		pass
+    if request.method == "GET":
+        pass
 
-	if request.method == "POST":
-		new_post = Posts(
-			name = request.form.get('name'),
-			content = request.form.get('post'),
-		)
-		db.session.add(new_post)
-		db.session.commit()
+    if request.method == "POST":
+        new_post = Posts(
+            name = request.form.get('name'),
+            content = request.form.get('post'),
+        )
+        db.session.add(new_post)
+        db.session.commit()
 
-	posts = Posts.query.all()
+    # Not working yet! HTML does not have a delete form action. Need to figure out how to differentiate a delete request with a post request and update HTML
+    if request.method == "DELETE":
+        post_id = request.form.get('postID')
+        print(post_id)
+        post_to_delete = Posts.query.filter_by(id='post_id').first()
+        db.session.delete(post_to_delete)
+        db.session.commit()
 
-	return render_template('index.html', posts=posts)	# looks into templates folder, finds the index.html file, and loads the template
+    posts = Posts.query.all()
+
+    return render_template('index.html', posts=posts)   # looks into templates folder, finds the index.html file, and loads the template
 
 # TO DO
 # Ensure blank entries are not added to the database (Name and Content cannot be None/Null)
@@ -44,4 +52,4 @@ def index():
 # Order posts from most recent to last 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
